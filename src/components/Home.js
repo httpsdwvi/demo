f280d317e82eb368dcb13793fe68d8647db7877a
httpsdwvi <davi.santos123@etec.sp.gov.br>
2023-01-13 00:52:28 -0300

    template criado

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,28 @@ import '../styles/Home.scss';
 
 const Home = () => {
     return (
-        <div></div>
+        <section className="signUp">
+            <div className="signUp__container">
+                <h3>Sign up for our newslatter</h3>
+                <form className="signUp__form">
+                    <label>email address</label>
+                        <div className="signUp__email">
+                            <input className="signUp__input" placeholder="Digite seu e-mail aqui"/>
+                            <button className="signUp__ok"> OK </button>
+                        </div>
+
+                        <div className="signUp__error">
+                            <p>ಥ_ಥ -- Por favor, digite um email válido </p>
+                        </div>
+
+
+                        <div className="signUp__success">
+                            <p>༼ つ ◕_◕ ༽つ -- Email válido</p>
+                        </div>
+                </form>
+            </div>
+        </section>
     )
 }
+
+export default Home
